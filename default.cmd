bf81871e6b67551906eacfa579d58112b0e1de01
Torsten Juul-Jensen <tjuuljensen@gmail.com>
2023-01-17 07:21:48 +0100

    script fix to use new name - ride.ps1

--- default.cmd
+++ default.cmd
@@ -1,1 +1,1 @@
-@powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0set-config.ps1" -include "%~dp0lib-windows.psm1" -preset "%~dpn0.preset"
+@powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0ride.ps1" -include "%~dp0lib-windows.psm1" -preset "%~dpn0.preset"
