c187a69f209122583374098001b81a9933752982
Torsten Juul-Jensen <tjuuljensen@gmail.com>
2023-02-16 18:43:06 +0100

    typo fixed

--- default.cmd
+++ default.cmd
@@ -5,7 +5,7 @@ rem Command to start RIDE configuration using default library and default preset
 rem The following are examples of other run profiles:
 
 rem RIDE using default library & default preset and with logging enabled
-rem @powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0ride.ps1" -include "%~dp0lib-windows.psm1" -preset "%~dpn0.preset" -log "%~dp0install-log.log" -ini "%~dp0config.ini"
+rem @powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0ride.ps1" -include "%~dp0lib-windows.psm1" -preset "%~dpn0.preset" -log "%~dp0install-log.log" 
 
 rem RIDE using default library, default preset, logging enabled and with loading of custom config from ini file 
 rem @powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0ride.ps1" -include "%~dp0lib-windows.psm1" -preset "%~dpn0.preset" -log "%~dp0install-log.log" -ini "%~dp0config.ini"
