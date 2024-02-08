af1d9faeaaf93189bb157ae0aaebbe06798b0026
Torsten Juul-Jensen <tjuuljensen@gmail.com>
2024-02-08 09:08:23 +0100

    NetVersion parameter added to receive both .net toolsets

--- components/files/Get-ZimmermanTools.cmd
+++ components/files/Get-ZimmermanTools.cmd
@@ -1,4 +1,4 @@
 @echo off
 rem Created to update Zimmerman Tools using Get-ZimmermanTools.ps1
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dpn0.ps1" -Dest %~dp0
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dpn0.ps1" -NetVersion 0 -Dest %~dp0
 
