69dd665226cb0ca9f6bcc76d6bc78b5413a1fef0
Torsten Juul-Jensen <tjuuljensen@gmail.com>
2024-02-14 05:51:29 +0100

    InstallADConnect added

--- lib/lib-DomainJoined.psm1
+++ lib/lib-DomainJoined.psm1
@@ -86,3 +86,49 @@ Function DisableFirewallPublic {
   }
   New-ItemProperty -path "HKLM:\SOFTWARE\Policies\Microsoft\WindowsFirewall\PublicProfile" -name "EnableFirewall" -Type DWORD -Value 0 -Force | Out-Null
 }
+
+
+function InstallADConnect {
+  # https://learn.microsoft.com/en-us/entra/identity/hybrid/connect/how-to-connect-install-roadmap#install-azure-ad-connect
+
+  Write-Output "###"
+  $SoftwareName = "Microsoft Entra Connect"
+  Write-Output "Installing $SoftwareName..."
+
+  # (Invoke-WebRequest -UseBasicParsing -Uri "https://www.microsoft.com/en-us/download/details.aspx?id=47594").Links.href  | Select-String -Pattern AzureADConnect
+  $FullDownloadURL = (Invoke-WebRequest -UseBasicParsing -Uri "https://www.microsoft.com/en-us/download/details.aspx?id=47594").Links.Href | Select-String -Pattern AzureADConnect | Select-Object -first 1
+  if (-not $FullDownloadURL) {
+      Write-Output "Error: $SoftwareName not found"
+      return
+  }
+
+  # Create bootstrap folder if not existing
+  $DefaultDownloadDir = (Get-ItemProperty -path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders")."{374DE290-123F-4565-9164-39C4925E467B}"
+  $BootstrapFolder = Join-Path -Path $DefaultDownloadDir -ChildPath "bootstrap"
+  if (-not (Test-Path -Path $BootstrapFolder)) {
+      New-Item -Path $BootstrapFolder -ItemType Directory | Out-Null
+  }
+
+  # Create software folder
+  $InvalidChars = [IO.Path]::GetInvalidFileNameChars() -join ''
+  $RegexInvalidChars = "[{0}]" -f [RegEx]::Escape($InvalidChars)
+  $SoftwareFolderName = $SoftwareName -replace $RegexInvalidChars
+  $SoftwareFolderFullName = Join-Path -Path $BootstrapFolder -ChildPath $SoftwareFolderName
+  if (-not (Test-Path -Path $SoftwareFolderFullName)) {
+      New-Item -Path $SoftwareFolderFullName -ItemType Directory | Out-Null
+  }
+
+  # Download software
+  Write-Output "Downloading file from: $FullDownloadURL"
+  $FileName = ([System.IO.Path]::GetFileName($FullDownloadURL).Replace("%20"," "))
+  $FileFullName = Join-Path -Path $SoftwareFolderFullName -ChildPath $FileName
+  Start-BitsTransfer -Source $FullDownloadURL -Destination $FileFullName
+  Write-Output "Downloaded: $FileFullName"
+
+  if (-not [Environment]::GetEnvironmentVariable("RIDEVAR-Download-Only", "Process")) {
+      # Install msi
+      Start-Process msiexec.exe -ArgumentList "/I ""$FileFullName"" /quiet" -Wait -NoNewWindow
+      Write-Output "Installation done for $SoftwareName"
+    }
+    
+}
